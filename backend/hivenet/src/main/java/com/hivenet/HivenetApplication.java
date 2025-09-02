e2d1ea85cb3d675aae3b128cbf9b2c112511c71e
chetann-verma <chetanverma2k4@gmail.com>
2025-09-02 23:16:04 +0530

    chore: setup of backend & frontend using Spring Initializr and Vite

---
+++ backend/hivenet/src/main/java/com/hivenet/HivenetApplication.java
@@ -0,0 +1,13 @@
+package com.hivenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HivenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HivenetApplication.class, args);
+	}
+
+}
---
+++ backend/hivenet/src/test/java/com/hivenet/HivenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hivenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
